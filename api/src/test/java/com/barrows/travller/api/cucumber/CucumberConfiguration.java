0efa82116c20c104573b3e20082f1e50c214dc7d
Jim Barrows <Jim.Barrows@gmail.com>
2025-05-07 22:55:09 -0500

    Set up externalized database configuration and import Testcontainers
    Added support for externalized configuration of the database using environment variables in application.yml and compose.yaml. Made TestcontainersConfiguration public and imported it in CucumberConfiguration to enhance testing setup. Adjusted IntelliJ project system files to reflect Gradle and Android changes.

--- api/src/test/java/com/barrows/travller/api/cucumber/CucumberConfiguration.java
+++ api/src/test/java/com/barrows/travller/api/cucumber/CucumberConfiguration.java
@@ -1,8 +1,10 @@
 package com.barrows.travller.api.cucumber;
 
 import com.barrows.travller.api.TestApiApplication;
+import com.barrows.travller.api.TestcontainersConfiguration;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 /**
  * Configuration class for Cucumber tests.
@@ -10,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
  */
 @CucumberContextConfiguration
 @SpringBootTest(classes = TestApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestcontainersConfiguration.class)
 public class CucumberConfiguration {
     // This class is intentionally empty. It's used only as a holder for the annotations.
 }
